2e4d81b7c74a4c99d39e6728d54537d0f926b544
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-11-04 05:47:01 -0600

    Add drop down function to mobile view.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-const forEach = require("for-each");
+(function dropdown() {
+  const dropDown = document.querySelectorAll(".dropdown");
 
-// document.getElementById("1").classList.toggle("show");
-const dropDown = document.querySelectorAll(".dropdown");
-
-dropDown.forEach((e) => {
-  e.addEventListener("click", () => {
-    e.classList.toggle("show");
+  dropDown.forEach((e) => {
+    e.addEventListener("click", () => {
+      e.classList.toggle("show");
+    });
   });
-});
-console.log("one");
+})();
+
+(function showMenu() {
+  const menuIcon = document.querySelector(".menu-icon");
+
+  menuIcon.addEventListener("click", () => {
+    console.log(menuIcon.src);
+    if (menuIcon.src === "http://../graphics/menu-icon.svg") {
+      menuIcon.src = "http://../graphics/close-icon.svg";
+    } else {
+      menuIcon.src = "http://../graphics/menu-icon.svg";
+    }
+  });
+
+  const menuItems = document.querySelector(".menu-items");
+
+  menuItems.classList.toggle("hidden");
+  // menuIcon.src = "/graphics/close.svg";
+
+  // if (menuIcon.textContent === "arrow_drop_down") {
+  //   menuIcon.textContent = "arrow_drop_up";
+  // } else {
+  //   menuIcon.textContent = "arrow_drop_down";
+  // }
+})();
